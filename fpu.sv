1a45290f9a2e2cae0e972822ad3233b4c75c074c
Hussien Mostafa <36410801+HMS-ELKHOLY@users.noreply.github.com>
2019-06-12 19:50:30 +0200

    Update fpu.sv

--- fpu.sv
+++ fpu.sv
@@ -1,3 +1,6 @@
+`include "multiplier/multi.sv"
+`include "adder/adder_floating_point/adder_floating_point.sv"
+`include "divider/fpu_division.sv"
 module fpu(input logic [1:0] funct ,
            input logic clk  ,
            input logic [31:0] a,b, 
