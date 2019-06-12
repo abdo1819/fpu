9f3b6452af7bf9de4bd8a82e00ef0b19dac4cbec
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-06-12 20:21:20 +0200

    add adder

--- fpu.sv
+++ fpu.sv
@@ -1,25 +1,33 @@
 `include "multiplier/multi.sv"
-
+`include "adder/adder_floating_point/adder_floating_point.sv"
 module fpu(
-    output logic [31:0] op,  //O/P
+    output logic [31:0] out,  //output
     input logic [31:0] a,
     input logic [31:0] b,
     input logic [1:0] s
     );
 
-logic [31:0] mu,ad,su,di;
+logic [31:0] mu,ad,sb,di;
 
 
-multi m(.a (a),.b (b),.op (mu));
 // .there(here)
+multi multiplier(.a (a),.b (b),.op (mu));
+adder_floating_point adder(.operand_normalized_ieee_a (a) ,
+                           .operand_normalized_ieee_b (b),
+                           .final_sum (ad));
+adder_floating_point subtractor(.operand_normalized_ieee_a (a) ,
+                           .operand_normalized_ieee_b ({~b[31],b[30:0]}),
+                           .final_sum (sb));
+
+
 always_comb
 begin
     
     case(s)
-        2'b00: op = mu;
-        2'b01: op = mu;
-        2'b10: op = mu;
-        2'b11: op = mu;
+        2'b00: out = ad;
+        2'b01: out = sb;
+        2'b10: out = mu;
+        2'b11: out = di;
     endcase
 
 end
